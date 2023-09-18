da57faf691ab148efefb1cc23561fb785bbeaa7a
rukkiecodes <rukkiecodes@gmail.com>
2023-09-18 11:44:50 +0100

    done with auth

---
+++ scripts/signin.js
@@ -0,0 +1,29 @@
+const email = document.querySelector('.email');
+const password = document.querySelector('.password');
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+    if (!email.value || !password.value) {
+        alert('Please complete the form and try again');
+    } else {
+        fetch('https://ecom-production-55db.up.railway.app/auth/signin', {
+            method: 'POST', // Use uppercase 'POST'
+            headers: {
+                "Content-Type": "application/json", // Correct header
+            },
+            body: JSON.stringify({
+                email: email.value,
+                password: password.value
+            })
+        })
+            .then(response => response.json()) // Assuming the response is JSON
+            .then(data => {
+                console.log(data); // Handle the response data here
+                localStorage.ecomUser = JSON.stringify(data)
+                location.replace('/dashboard.html')
+            })
+            .catch(err => {
+                console.error(err); // Handle any errors
+            });
+    }
+});
---
+++ scripts/signup.js
@@ -0,0 +1,31 @@
+const name = document.querySelector('.name');
+const email = document.querySelector('.email');
+const password = document.querySelector('.password');
+const button = document.querySelector('button');
+
+
+button.addEventListener('click', () => {
+    if (!name.value || !email.value || !password.value) {
+        alert('Please complete the form and try again');
+    } else {
+        fetch('https://ecom-production-55db.up.railway.app/auth/signup', {
+            method: 'POST', // Use uppercase 'POST'
+            headers: {
+                "Content-Type": "application/json", // Correct header
+            },
+            body: JSON.stringify({
+                name: name.value,
+                email: email.value,
+                password: password.value
+            })
+        })
+            .then(response => response.json()) // Assuming the response is JSON
+            .then(data => {
+                localStorage.ecomUser = JSON.stringify(data)
+                location.replace('/index.html')
+            })
+            .catch(err => {
+                console.error(err); // Handle any errors
+            });
+    }
+});
