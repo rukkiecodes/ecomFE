b3c4f05e22408ef6c345f94d4b580d562c7f007a
rukkiecodes <rukkiecodes@gmail.com>
2023-09-29 12:41:51 +0100

    done

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -3,6 +3,8 @@ const closeButton = document.querySelector('aside .buttonDiv button')
 
 const aside = document.querySelector('aside')
 
+const goods = document.querySelector('.dashboardMain .cards')
+
 menu.addEventListener('click', () => {
     aside.classList.add('openAside')
     aside.classList.remove('aside')
@@ -11,4 +13,30 @@ menu.addEventListener('click', () => {
 closeButton.addEventListener('click', () => {
     aside.classList.remove('openAside')
     aside.classList.add('aside')
-})
+});
+
+(async () => {
+    fetch('https://ecom-production-c7d8.up.railway.app/trade/get', {
+        method: 'get', // Use uppercase 'POST'
+        headers: {
+            "Content-Type": "application/json", // Correct header
+        }
+    })
+        .then(response => response.json()) // Assuming the response is JSON
+        .then(response => {
+            // console.log(response)
+            response?.goods?.forEach(item => {
+                goods.innerHTML += `<div class="card">
+                <div class="imageHolder">
+                    <img src="${item?.image}" />
+                </div>
+                <p class="itemName">${item?.name}</p>
+                <p class="itemDescription">${item?.description}</p>
+                <button>Add to Cart</button>
+            </div>`
+            });
+        })
+        .catch(err => {
+            console.error(err); // Handle any errors
+        });
+})()
